bdbc2d66e3dfd1d73f3dd102ed4e93ea77e541c8
Anh Tu <leantu2004@gmail.com>
2025-10-21 05:38:24 +0700

    push

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ limit: '100mb', extended: true }))
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://minhdental.com',
+      'https://duoclieuphattam.com',
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
